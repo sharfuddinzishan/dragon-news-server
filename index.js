bb756311ca063342caf061732eeb0d6f5e701854
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-01-26 05:04:11 +0600

    Completed Dragon News Server

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Dragon News')
 })
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id
+    let getCatNews = []
+    if (id == 08)
+        getCatNews = news
+    else
+        getCatNews = news.filter(news => news.category_id == id)
+    res.send(getCatNews)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    let selectedNews = []
+    selectedNews = news.find(singleNews => singleNews._id == id)
+    res.send(selectedNews)
 })
 
 
